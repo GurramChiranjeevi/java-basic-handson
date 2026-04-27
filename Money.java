da5d15fa59616f7c31911b52d6601713b219f79b
Chiranjeevi Gurram <chiranjeevigurram@Chiranjeevis-MacBook-Pro.local>
2026-04-27 15:11:28 +0530

    implements money class with add, minus functionality

--- Money.java
+++ Money.java
@@ -1,1 +1,17 @@
 // Money.java
+
+class Money{
+  final int value;
+  Money(int value) {
+    this.value = value;
+  }
+
+  Money add(Money rupee) {
+    return new Money(this.value + rupee.value);
+  }
+
+  Money minus(Money rupee) {
+
+    return new Money(this.value - rupee.value);
+  }
+}
--- StringUtils.java
+++ StringUtils.java
@@ -1,1 +1,7 @@
-// StringUtils
+class StringUtils {
+  static boolean containsChar(String word, char target) {
+    System.out.println(word[0]);
+    return true;
+  }
+
+}
